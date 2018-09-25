c43337a2a6b9924c376844cb441ebbdd00ef328b
Crispin Wellington <retrogradeorbit@gmail.com>
2018-09-25 13:56:12 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject syncsnap "0.1.0-SNAPSHOT"
+(defproject syncsnap "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
